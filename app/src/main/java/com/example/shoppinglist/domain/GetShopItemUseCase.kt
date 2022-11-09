97c4e40998c12f0eb4c069f72ca270919d3830f3
Dmitrii <actovus@gmail.com>
2022-11-09 16:08:46 +0300

    create app

---
+++ app/src/main/java/com/example/shoppinglist/domain/GetShopItemUseCase.kt
@@ -0,0 +1,7 @@
+package com.example.shoppinglist.domain
+
+data class GetShopItemUseCase(
+    fun getShopItem(shopItemId: Int): ShopItem {
+        TODO()
+    }
+)
---
+++ app/src/main/java/com/example/shoppinglist/domain/GetShopListUseCase.kt
@@ -0,0 +1,7 @@
+package com.example.shoppinglist.domain
+
+data class GetShopListUseCase(
+    fun getShopList(): List<ShopItem> {
+        TODO()
+    }
+)
